da1051a30b3c5a08401ff59aef6ce9429af28119
Yeferson David Cano H <canocs3@live.com>
2026-03-02 21:24:42 -0500

    Add class-variance-authority and tailwind-merge dependencies; refactor Button component to use CVA for styling; introduce utility function for class name merging

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,9 @@
+import { cva } from "class-variance-authority";
+import { cn } from "../../lib/utils";
+
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: "primario" | "secundario" | "outlined" | "danger" | "custom";
-  size?: "small" | "large" | "extra-large";
+  variant?: "primario" | "secundario" | "outlined" | "danger" | "custom";
+  size?: "xs" | "sm" | "lg";
   icon?: boolean;
   icono?: React.ReactNode;
   onlyIcon?: boolean;
@@ -9,39 +12,45 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({
   children,
   variant = "primario",
-  size = "large",
+  size,
   icon = false,
   icono,
   onlyIcon = false,
-  className = "",
+  className,
   ...props
 }: Props) => {
-  const baseStyles =
-    "transition-all duration-200 ease-out rounded-md flex items-center justify-center cursor-pointer font-medium focus:outline-none focus:ring-2 focus:ring-opacity-50 active:scale-95";
+  const buttonVariants = cva(
+    "transition-all duration-200 ease-out rounded-md flex items-center justify-center cursor-pointer font-medium focus:outline-none focus:ring-2 focus:ring-opacity-50 active:scale-95",
+    {
+      variants: {
+        variant: {
+          primario: "bg-emerald-700 text-textbtn text-white ",
+          secundario:
+            "bg-accent text-text hover:bg-accent/90 focus:ring-accent/50 border border-accent/10",
+          outlined:
+            "border border-gray-300 text-text hover:bg-gray-50 focus:ring-gray-300/50",
+          danger:
+            "bg-red-600 text-white hover:bg-red-700 focus:ring-red-600/50",
+          custom: "",
+        },
+        size: {
+          default: onlyIcon ? "w-8 h-8" : "h-8 px-3 text-base flex gap-2",
+          xs: onlyIcon ? "w-6 h-6" : "h-6 px-2 text-xs flex gap-2",
+          sm: onlyIcon ? "w-9 h-9" : "h-7 px-4 text-sm flex gap-2",
+          lg: onlyIcon ? "w-11 h-11" : "h-9 px-6 text-base flex gap-2",
+        },
+      },
 
-  // Tamaños siguiendo estándares MIT Touch Lab
-  const sizeVariants = {
-    small: onlyIcon ? "w-8 h-8" : "h-8 px-3 text-sm",
-    large: onlyIcon ? "w-9 h-9" : "h-9 px-4 text-base",
-    "extra-large": onlyIcon ? "w-11 h-11" : "h-11 px-6 text-lg",
-  };
-
-  const spacing = onlyIcon ? "" : "gap-2";
-
-  const variants = {
-    primario: "bg-emerald-700 text-textbtn text-white ",
-    secundario:
-      "bg-accent text-text hover:bg-accent/90 focus:ring-accent/50 border border-accent/10",
-    outlined:
-      "bg-transparent text-primary border border-primary hover:bg-primary/5 focus:ring-primary/50",
-    danger:
-      "bg-red-500 text-white hover:bg-red-600 focus:ring-red-500/50 border border-red-500/10",
-    custom: "",
-  };
+      defaultVariants: {
+        variant: "primario",
+        size: "default",
+      },
+    },
+  );
 
   return (
     <button
-      className={`${baseStyles} ${sizeVariants[size]} ${variants[variant]} ${spacing} ${className}`}
+      className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     >
       {icon && <span>{icono}</span>}
